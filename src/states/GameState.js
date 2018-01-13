b13d6e355b1131f41235aae5334904ce3fc3d1eb
Dan Hett <hellodanhett@gmail.com>
2018-01-13 19:21:02 +0000

    Fixed hand motion, added buttons

--- src/states/GameState.js
+++ src/states/GameState.js
@@ -6,26 +6,34 @@ class GameState extends Phaser.State {
     create() {
       this.bg = this.game.add.sprite(0, 0, "game-background");
 
-      this.right = this.game.add.sprite(598, 34, "hand-right");
+      this.xtarget = this.game.input.x;
+      this.ytarget = this.game.input.y;
+      this.xdiff;
+      this.ydiff;
+
+      // create the hands
+      this.right = this.game.add.sprite(500, 34, "hand-right");
       this.left = this.game.add.sprite(212, 331, "hand-left");
 
+      // adjust the anchor point of the left hand to
+      // match the fingertip, so we can stick to the mouse
       this.left.anchor.set(0.68, 0.05);
-
-      this.game.physics.enable(this.left, Phaser.Physics.ARCADE);
     }
 
     update() {
+      this.moveHand();
+    }
 
-      this.left.position.x = this.game.input.x;
-      this.left.position.y = this.game.input.y;
+    moveHand() {
+      // rotate the hand slightly based on the position
+      this.left.angle = this.game.input.x / 40;
 
-      /*
-      this.game.physics.arcade.moveToPointer(this.left, 1000);
+      // move the hand to the cursor, with a little easing
+      this.xdiff = this.game.input.x - this.left.position.x;
+      this.left.position.x += this.xdiff *= 0.2;
 
-      if (Phaser.Rectangle.contains(this.left.body, this.game.input.x, this.game.input.y)) {
-        this.left.body.velocity.setTo(0, 0);
-      }
-      */
+      this.ydiff = this.game.input.y - this.left.position.y;
+      this.left.position.y += this.ydiff *= 0.2;
     }
 
     shutdown() {
